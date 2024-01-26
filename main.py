ba5b7f12ca8eab6aee58c3a0d3455ae71be5edbb
yashrshinde <yashrshinde03@gmail.com>
2024-01-26 15:40:26 +0530

    Changes Committed

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from MLProject import logger
+from MLProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Hello")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
